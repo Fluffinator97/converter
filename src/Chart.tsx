d094a1e792aad6b9157b7181f219de7ff5220547
Kristóf <krstf456@gmail.com>
2020-03-08 21:47:22 +0100

    currencies added and some other basics to the chart

--- src/Chart.tsx
+++ src/Chart.tsx
@@ -1,13 +1,39 @@
 import React, {Component} from "react"
 import {Line} from 'react-chartjs-2'
 
-class  Chart extends Component{
+interface Props {
+
+}
+
+interface State {
+
+}
+export default class Chart extends React.Component<Props, State>{
+    constructor(props: Props){
+        super(props)
+        this.state = {
+            chartData:{
+                labels: ['Euro', 'Dollar'],
+                datasets:[
+                    {
+                        label: 
+                    }
+                ]
+            }
+        }
+    }
     render(){
         return(
             <div className="chart">
-                CHART COMPONENT
+                <Bar
+                    data={data}
+                    width={100}
+                    height={50}
+                    options={{
+                    maintainAspectRatio: false
+                    }}
+                />
             </div>
         )
     }
 }
-export default Chart
