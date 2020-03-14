f1aa1be6ba09461143a1d8e22855fefef3db9d2c
mamta srinivas <mamtasrinivassp@gmail.com>
2020-03-14 20:41:19 +0100

    working w/0 dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -5,32 +5,32 @@ import { Switch, Route, Link } from 'react-router-dom'
 export default function App() {
   return (
     <div className="App">
-      <nav style={wrapper}>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/graph">Graph</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
+    <nav style={wrapper}>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/graph">Graph</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
 
-      <Switch>
-        <Route path="/">
-          <Layout displayPage={'home'} />
-        </Route>
-        <Route exact path="/graph">
-          <Layout displayPage={'graph'} />
-        </Route>
-        <Route path="/about">
-          <Layout displayPage={'about'} />
-        </Route>
-      </Switch>
-    </div>
+    <Switch>
+      <Route path="/graph">
+        <Layout displayPage={'graph'} />
+      </Route>
+      <Route path="/about">
+      <Layout displayPage={'about'} />
+      </Route>
+      <Route path="/">
+        <Layout displayPage={'home'} />
+      </Route>
+    </Switch>
+  </div>
 
   )
 }
--- src/LineGraph.tsx
+++ src/LineGraph.tsx
@@ -4,36 +4,36 @@ import Chart from "chart.js";
 
 interface Props {
     toCurrency: string,
-fromCurrency:string
+    fromCurrency: string
 }
 
 interface State {
     today: string,
-    lastMonth: string, 
+    lastMonth: string,
     values: number[],
     tags: string[]
 }
 
-export default class LineGraph extends React.Component<Props,State>{
-    constructor(props: Props){
-       
+export default class LineGraph extends React.Component<Props, State>{
+    constructor(props: Props) {
+
         super(props)
-         let today = new Date()
-         
+        let today = new Date()
+
         this.state = {
-            today : today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate(),
-            
+            today: today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate(),
+
             lastMonth: today.getFullYear() + '-' + (today.getMonth()) + '-' + today.getDate(),
 
             values: [],
-            tags:[]
-     
-            
+            tags: []
+
+
         }
     }
-    
+
     chartRef = React.createRef() as any;
-    
+
     componentDidMount() {
         console.log('MOUNT')
         this.fetchData()
@@ -41,32 +41,32 @@ export default class LineGraph extends React.Component<Props,State>{
 
     fetchData() {
         fetch(`https://api.exchangeratesapi.io/history?start_at=${this.state.lastMonth}&end_at=${this.state.today}&base=${this.props.fromCurrency}`)
-        .then(res => 
-        
+            .then(res =>
+
                 res.json()
             )
-        .then(data => {
-            let rates: any = Object.values(data.rates)
-            
-            const values: number[] = []
-            for (const rate of rates) {  
-                values.push(rate[this.props.toCurrency])
-            }
-            this.setState({values: values, tags: this.getLabel(values)})
-           
-            
-        }).catch(error => {
-            console.log(error);
-    
-        });
+            .then(data => {
+                let rates: any = Object.values(data.rates)
+
+                const values: number[] = []
+                for (const rate of rates) {
+                    values.push(rate[this.props.toCurrency])
+                }
+                this.setState({ values: values, tags: this.getLabel(values) })
+
+
+            }).catch(error => {
+                console.log(error);
+
+            });
     }
 
-    getLabel(values: number[]){
+    getLabel(values: number[]) {
         const test: string[] = []
-            for(const item of values){
+        for (const item of values) {
             test.push('.')
         }
-        test.splice(0,2)
+        test.splice(0, 2)
         test.unshift(this.state.lastMonth)
         test.push(this.state.today)
         return test
@@ -81,7 +81,7 @@ export default class LineGraph extends React.Component<Props,State>{
             const myChartRef = this.chartRef.current.getContext("2d");
             new Chart(myChartRef, {
                 type: "line",
-                
+
                 data: {
                     labels: this.state.tags,
                     datasets: [
@@ -92,7 +92,7 @@ export default class LineGraph extends React.Component<Props,State>{
                     ]
                 },
                 options: {
-                    events:['null']
+                    events: ['null']
                 }
             });
         }
